66045ef248d4fb4b6b04ce3c71f33c7576282f97
Ninja <faqsax@gmx.net>
2017-01-13 13:21:04 +0100

    added documentation, optimised unit tests
    added documentation for tokenizer
    optimised unittests for tokenizer

--- src/main.rs
+++ src/main.rs
@@ -17,33 +17,38 @@ fn main() {
     
 }
 
-// tokenization
+//tokenization
 
 //function for returning a vector of whitespace-splitted words for a given string
 fn split_string(buf: &str) -> (Vec<&str>) {
 	return buf.split_whitespace().collect();
 }
 
-// struct for iterator
-struct Stringsplitter {
+//struct for iterator
+struct Tokenizer {
 	strings: Vec<String>,
 }
 
-//return a vector for each string of the given vector
-impl Iterator for Stringsplitter {
+//returns a vector for each string of the given vector
+impl Iterator for Tokenizer {
 	type Item = Vec<String>;
 	
 	fn next(&mut self) -> Option<Vec<String>> {
 		if self.strings.len() <= 0 {
 			None
 		}else {
+			//1. get the last element of the vec "strings" and remove it
+			//2. unwrap returned value to get String
+			//3. split_whitespace to get rid of whitespaces, returns iterator
+			//4. iterator values getting owned (&str -> String)
+			//5. collects all values and returns them in an Vec (Vec<String>)
 			Some(self.strings.pop().unwrap().split_whitespace().map(ToOwned::to_owned).collect())
 		}
 	}
 }
 
 
-// normalization to lowercase (vec<String> to x strings)
+//normalization to lowercase (vec<String> to x strings)
 struct ToLowerCase {
 	strings: Vec<String>
 }
@@ -63,7 +68,7 @@ impl Iterator for ToLowerCase {
 #[cfg(test)]
 mod tests {
 	use super::split_string;
-	use super::Stringsplitter;
+	use super::Tokenizer;
 	
    
     #[test]
@@ -84,13 +89,15 @@ mod tests {
 	}
 	
 	#[test]
-	fn split_iterator_test() {
+	fn tokenizer_test() {
 		let mut test = vec!["Das ist ein Haus".to_owned(), "Das ist ein Boot".to_owned(), "Das ist ein Auto".to_owned()];
-		let mut iter = Stringsplitter {strings: test};
-		//let result = iter.collect::<Vec<Vec<String>>>();
-		assert_eq!(iter.next().unwrap(), vec!["Das".to_owned(), "ist".to_owned(), "ein".to_owned(), "Auto".to_owned(), ]);
-		assert_eq!(iter.next().unwrap(), vec!["Das".to_owned(), "ist".to_owned(), "ein".to_owned(), "Boot".to_owned(), ]);
-		assert_eq!(iter.next().unwrap(), vec!["Das".to_owned(), "ist".to_owned(), "ein".to_owned(), "Haus".to_owned(), ]);
-		assert_eq!(iter.next(), None);
+		let result = (Tokenizer {strings: test}).collect::<Vec<Vec<String>>>();
+		for stringvec in result {
+			for string in stringvec {
+				for char in string.chars() {
+					assert_eq!(char.is_whitespace(), false);
+				}
+			}
+		}
 	}
 }
