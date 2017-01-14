ec0be1da7ca7356e0f1c8ec607d82c71cc9402b5
mæp <m.aep@live.com>
2017-01-14 02:12:30 +0100

    hurr durr

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,57 @@
 use std::io::BufRead;
 use std::io;
+use std::str::SplitWhitespace;
+
+fn vsplit(s: &String) -> Vec<&str> {
+    let mut v: Vec<&str> = vec![];
+    for word in s.split_whitespace() {
+        v.push(word);
+    }
+    return v;
+}
+
+// das geht doch besser oder?
+// python:
+// def isplit(s):
+//     for w in s.split():
+//         yield w
+// oder sogar:
+// def isplit(s):
+//     yield from s.split()
+
+// das 'a is voll bescheuert!!!!!
+struct Isplit<'a> {
+    sw: SplitWhitespace<'a>
+}
+
+//   +-- warum muss das 'a dahin?
+//   v           v- und nicht hier hin?
+impl<'a> Iterator for Isplit<'a> {
+    type Item = &'a str;
+    fn next(&mut self) -> Option<&'a str> {
+        self.sw.next()
+    }
+}
+
+fn isplit(s: &String) -> Isplit {
+    Isplit { sw: s.split_whitespace() }
+}
 
 fn main() {
     let stdin = io::stdin();
-    //For each line from stdin
     for line in stdin.lock().lines() {
-        //Check if the line was correctly read
-        if let Ok(line) = line {
-            //Split the line at every whitespace
-            //Then Print the terms
-            //Iterators in rust are lazy. So the count at the end is only to consume the iterator!
-            line.split_whitespace().inspect(|term| println!("{}", term)).count();
+        let ln = line.unwrap();
+        for word in isplit(&ln) {
+            println!("{}", word);
         }
     }
-    
 }
 
 
 #[cfg(test)]
 mod tests {
-   
     #[test]
     fn basic_test() {
-        //use asserts to check your results
-        //Use assert to check for a true value
-        assert!(true);
-        //Or with an expression
-        assert!(1 == 1);
-        //Or use assert_eq! for a check of equality
-        assert_eq!(1, 1);
     }
 
 }
